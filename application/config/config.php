bbc3ca2771470eab244898bee4f2fd6a85c554ac
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-10-06 14:24:20 +0700

    menambahkan zona waktu indonesia

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+/*Setting waktu indonesia
+*/
+date_default_timezone_set("Asia/Jakarta");
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL

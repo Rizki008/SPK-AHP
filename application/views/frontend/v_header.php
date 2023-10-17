4deb987dc594637413ce9e922777bbacfa38e763
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-10-17 15:31:55 +0700

    ubah header

--- application/views/frontend/v_header.php
+++ application/views/frontend/v_header.php
@@ -2,10 +2,17 @@
 	<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
 		<div class="sidenav-header">
 			<i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-			<a class="navbar-brand m-0" href=" <?= base_url('sales') ?> " target="_blank">
-				<img src="<?= base_url() ?>template/assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-				<span class="ms-1 font-weight-bold text-white">Telkom Indonesia</span>
-			</a>
+			<?php if ($this->session->userdata('level_user') == 'pimpinan') { ?>
+				<a class="navbar-brand m-0" href=" <?= base_url('pimpinan') ?> ">
+					<img src="<?= base_url() ?>template/assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+					<span class="ms-1 font-weight-bold text-white">Telkom Indonesia</span>
+				</a>
+			<?php } else { ?>
+				<a class="navbar-brand m-0" href=" <?= base_url('sales') ?> ">
+					<img src="<?= base_url() ?>template/assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+					<span class="ms-1 font-weight-bold text-white">Telkom Indonesia</span>
+				</a>
+			<?php } ?>
 		</div>
 		<hr class="horizontal light mt-0 mb-2">
 		<div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
@@ -60,6 +67,18 @@
 								<i class="material-icons opacity-10">receipt_long</i>
 							</div>
 							<span class="nav-link-text ms-1">Analisis AHP</span>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white <?php if (
+															$this->uri->segment(1) == 'pimpinan' && $this->uri->segment(2) == 'akun'
+														) {
+															echo "active bg-gradient-primary";
+														} ?> " href="<?= base_url('pimpinan/akun') ?>">
+							<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+								<i class="material-icons opacity-10">person</i>
+							</div>
+							<span class="nav-link-text ms-1">Akun Sales</span>
 						</a>
 					</li>
 				<?php } else { ?>
